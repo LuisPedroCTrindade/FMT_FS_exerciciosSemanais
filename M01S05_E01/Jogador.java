972ebc9b2143785350f30a64680218e3d78f6d53
LuisPedroCTrindade <luispedrotrindade77@gmail.com>
2024-02-08 11:44:59 -0300

    commit inicial - add o exercicio 1 da semana 5

---
+++ M01S05_E01/Jogador.java
@@ -0,0 +1,15 @@
+public class Jogador {
+    String nome;
+    int idade;
+    int pontuacao;
+    int numeroTentativas;
+
+    public void adicionaPontos() {
+    }
+
+    public void perdePontos() {
+    }
+
+    public void adicionaTentativa() {
+    }
+}
