3e3a18464153fa76d198f2f66114957a73ab8583
LuisPedroCTrindade <luispedrotrindade77@gmail.com>
2024-03-19 10:27:42 -0300

    add S08

---
+++ Modulo 1/Semana 8/M1S08/src/test/java/br/com/FMT/fullstack/education/M1S08/M1S08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.FMT.fullstack.education.M1S08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
