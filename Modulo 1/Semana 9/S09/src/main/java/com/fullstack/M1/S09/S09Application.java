dd61fd391e159fdd89e9c7293359fb17b868ff3c
LuisPedroCTrindade <luispedrotrindade77@gmail.com>
2024-04-09 14:47:34 -0300

    M1S09 - Exercicios da semana.

---
+++ Modulo 1/Semana 9/S09/src/main/java/com/fullstack/M1/S09/S09Application.java
@@ -0,0 +1,13 @@
+package com.fullstack.M1.S09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S09Application.class, args);
+	}
+
+}
